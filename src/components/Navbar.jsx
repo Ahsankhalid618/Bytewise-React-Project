5d0fabd740c4da0935c9c02055b0fb01d01b4056
Ahsan khalid <ahsankhalid816@gmail.com>
2024-07-28 23:51:04 +0500

    project bytewise  completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
-import logo from "../assets/Logo.svg"
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import logo from "../assets/Logo.svg";
+
 export const Navbar = () => {
-    return (
-        <div className='bg-[#F5F7FA]'>
-            <div className='py-5 px-5 flex items-center justify-between container mx-auto'>
-                <div className='w-[20%]'>
-                    <img src={logo} alt="logo" />
-                </div>
-                <div className='w-[55%]'>
-                    <ul className='flex items-center gap-12'>
-                        <li className='font-medium'>Home</li>
-                        <li>Service</li>
-                        <li>Feature</li>
-                        <li>Product</li>
-                        <li>Testimonial</li>
-                        <li>FAQ</li>
-                    </ul>
-                </div>
-                <div className='w-[18%] flex gap-2'>
-                    <button className='tertiary-btn'>Login</button>
-                    <button className='primary-btn-small text-sm font-medium'>Sign up</button>
-                </div>
-            </div>
+  return (
+    <div className="bg-[#F5F7FA]">
+      <div className="py-5 px-5 flex items-center justify-between container mx-auto">
+        <div className="w-[20%] cursor-pointer">
+          <img src={logo} alt="logo" />
         </div>
-    )
-}
+        <div>
+          <ul className="flex items-end gap-10">
+            <li className="font-semibold text-gray-500 cursor-pointer hover:text-gray-800">
+              Home
+            </li>
+            <li className="font-semibold text-gray-500 cursor-pointer hover:text-gray-800">
+              Features
+            </li>
+            <li className="font-semibold text-gray-500 cursor-pointer hover:text-gray-800">
+              Community
+            </li>
+            <li className="font-semibold text-gray-500 cursor-pointer hover:text-gray-800">
+              Blog
+            </li>
+            <li className="font-semibold text-gray-500 cursor-pointer hover:text-gray-800">
+              Pricing
+            </li>
+          </ul>
+        </div>
+        <div className="w-[18%] flex gap-2">
+          <button className=" px-8 py-3 text-white rounded-md bg-[#45d449] hover:bg-[#4be751] text-sm font-medium">
+            Register Now --&gt;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
